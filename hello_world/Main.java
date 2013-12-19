2450f8dd3f1a6a7f3305ab93000433626c567895
Sergey Popov <poserg@gmail.com>
2013-12-19 18:19:55 +0400

    Add solution by java

---
+++ hello_world/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] argv) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+        System.out.println("Hello world!");
+    }
+}
